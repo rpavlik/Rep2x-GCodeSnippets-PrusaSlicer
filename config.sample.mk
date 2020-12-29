e049d70f70d44798a72f4550037d19e7f59cafee
Ryan Pavlik <ryan@ryanpavlik.com>
2020-12-29 14:36:25 -0600

    Update sample config.

--- config.sample.mk
+++ config.sample.mk
@@ -2,5 +2,10 @@
 # Copy this config.sample.mk to config.mk and edit these as appropriate.
 
 SCRIPT_PATH    := /You-need-to-update-print-configs/or-use-config.mk/to-specify-path-to/make_fcp_x3g
+
+# Windows sample
+#SCRIPT_PATH    := c\:/users/YOURUSERNAME/scoop/apps/busybox/current/busybox.exe bash c\:/users/YOURUSERNAME/Desktop/src/Rep2x-GCodeSnippets-PrusaSlicer/make_fcp_x3g
+
+# Uncomment to insert, for PrusaSlicer 2.2 and earlier.
 OCTOPRINT_HOST := http://octopi.local/
 OCTOPRINT_KEY  := YOUR_API_KEY_HERE 
